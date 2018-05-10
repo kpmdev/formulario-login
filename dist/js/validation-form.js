629efe9b64f0fd0228077bbce3cbc05a357fe949
Kevin Paternina <kevin.paternina14@hotmail.com>
2018-05-09 20:36:08 -0500

    Iniciando el proyecto

---
+++ dist/js/validation-form.js
@@ -0,0 +1,58 @@
+const containerNotificacion = document.querySelector('.notification'),
+	inputName = document.getElementById('inputName'),
+	inputEmail = document.getElementById('inputEmail');
+
+const allInputs = document.querySelectorAll('.input');
+const form = document.getElementById('form-registry');
+const exprCamposVacios = /[a-z]/;
+
+const exprEmail       = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+const caracteres  = /^\w+([a-zA-Z])$/;
+
+
+form.addEventListener('submit', (e) =>{
+	containerNotificacion.classList.add('notification--is-visible');
+
+	if(allInputs[0].value.length === 0 || allInputs[1].value.length === 0){
+		containerNotificacion.innerHTML = `<p class=\"notification__error\">Todos los campos son obligatorios.</p> 
+			<div class=\"notification__close\"> </div>`;
+		e.preventDefault();
+		inputName.focus();
+	}
+	
+	else if(inputName.value.length <3){
+		containerNotificacion.innerHTML = `<p class=\"notification__error\">El nombre es demasiado corto.</p> 
+			<div class=\"notification__close\"> </div>`;
+		e.preventDefault();
+		inputName.focus();
+	}
+	else if(!caracteres.test(inputName.value)){
+		containerNotificacion.innerHTML = `<p class=\"notification__error\">El nombre no puede contener números o caracteres especiales.</p> 
+			<div class=\"notification__close\"> </div>`;
+		e.preventDefault();
+		inputName.focus();
+	}
+	else if(!exprEmail.test(inputEmail.value)){
+		containerNotificacion.innerHTML = `<p class=\"notification__error\"> ${inputName.value.toLowerCase()}, el email no tiene un formato adecuado.</p> 
+			<div class=\"notification__close\"> </div>`;
+		e.preventDefault();
+		inputEmail.focus();
+	}
+	else{
+		containerNotificacion.innerHTML = `<p class=\"notification__error\"> Todo bien, todo correcto, y yo que me alegro.</p>
+			<div class=\"notification__close\"> </div>`;
+		//e.preventDefault();
+	}
+})
+
+document.addEventListener('click', (e) =>{
+	if(e.target.className === 'notification__close'){
+		containerNotificacion.classList.remove('notification--is-visible');
+	}
+})
+
+document.addEventListener('keyup', (e)=>{
+	if(e.key === 'Escape'){
+		containerNotificacion.classList.remove('notification--is-visible');
+	}
+})
